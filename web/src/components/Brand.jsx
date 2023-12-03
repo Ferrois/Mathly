def33017f05e0ed5cd68c158866a3a75a2270c60
Ferrois <ferrois2005@gmail.com>
2023-12-04 01:54:22 +0800

    --modify: brand sizing

--- web/src/components/Brand.jsx
+++ web/src/components/Brand.jsx
@@ -4,8 +4,8 @@ import MathlySvg from "@/assets/Mathly.svg";
 export default function Brand({ className }) {
   return (
     <div className={"flex items-center gap-2 " + className}>
-      <Image src={MathlySvg} alt="Mathly Logo" width={40} height={40} />
-      <span className="text-2xl">Mathly</span>
+      <Image src={MathlySvg} alt="Mathly Logo" width={35} height={35} />
+      <span className="text-xl">Mathly</span>
     </div>
   );
 }
